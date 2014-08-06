5c42d8d6463d9d44d3783c700d6294bb9648feca
Tatarao Voleti <tatarao.voleti@gmail.com>
2014-08-06 12:28:22 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirlsbangalore::Application.config.secret_key_base = '884d9c5b79f2325cbddc40a773f4df79fb17059f0b33d10206cd52f61f9d615e4225af30caca12800bbf029fd88d11538141a21c77f27fea7a44718ec1262fe3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railsgirlsbangalore::Application.config.session_store :cookie_store, key: '_railsgirlsbangalore_session'
